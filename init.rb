40a6830f1c9a0ca97fd7500126c3cee821fa894d
Sergo8707 <sergo8707@gmail.com>
2017-03-11 13:01:02 +0200

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :sergo do
+  name 'Sergo plugin'
+  author 'Sergiy Drach'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/Sergo8707/plugin-for-Redmine'
+  author_url 'https://github.com/Sergo8707'
+end
