4c7c9657825acbd0100fce2e599020c25d315323
Sergo8707 <sergo8707@gmail.com>
2017-03-14 20:54:14 +0200

    refactoring

--- init.rb
+++ init.rb
@@ -1,5 +1,12 @@
 require 'redmine'
 
+require_dependency 'task_update_listener'
+
+Rails.application.config.to_prepare do
+  AttachmentsController.send :include, TaskUpdateWatcher::AttachmentPatch
+  IssueRelationsController.send :include, TaskUpdateWatcher::IssueRelationsPatch
+end
+
 Redmine::Plugin.register :link_plugin do
   name 'Sergo plugin'
   author 'Sergiy Drach'
@@ -8,7 +15,6 @@ Redmine::Plugin.register :link_plugin do
   url 'https://github.com/Sergo8707/plugin-for-Redmine'
   author_url 'https://github.com/Sergo8707'
 
-  settings \
-    default: { 'empty' => true },
-    partial: 'settings/settings'
+  settings default: { 'request_url' => 'http://httpbin.org/post' },
+           partial: 'settings/settings'
 end
